e362e05cbf86e11d0ee18a724ec4773043dc2cd3
chjiahao <1294979952@qq.com>
2023-12-14 23:19:27 +0800

    身高体重标准值窗口

--- src/views/Life/components/Standard.vue
+++ src/views/Life/components/Standard.vue
@@ -1,5 +1,29 @@
 <template>
     <div class="wrapper">
+        <div class="child-table">
+            <div class="table-content">
+                <div class="item man-sex">男</div>
+                <div class="item head-man-height">身高</div>
+                <div class="item head-man-width">体重</div>
+            </div>  
+            <div class="table-content" v-for="(item,index) in childList.manList">
+                <div class="item man-sex">{{ item.year }}</div>
+                <div class="item height">{{ item.height }}</div>
+                <div class="item width">{{ item.weight }}</div>
+            </div>  
+        </div>
+        <div class="child-table">
+            <div class="table-content">
+                <div class="item woman-sex">女</div>
+                <div class="item head-woman-height">身高</div>
+                <div class="item head-woman-width">体重</div>
+            </div>  
+            <div class="table-content" v-for="(item,index) in childList.womanList">
+                <div class="item woman-sex">{{ item.year }}</div>
+                <div class="item height">{{ item.height }}</div>
+                <div class="item width">{{ item.weight }}</div>
+            </div> 
+        </div>
     </div>
 </template>
 <script>
@@ -9,6 +33,7 @@ export default {
 
     data() {
         return {
+            childList:{},
         }
     },
     watch: {
@@ -22,7 +47,8 @@ export default {
         async getCount() {
             const data = await getStandardBody()
             if (data) {
-                console.log(data, 'Standarddata----------');
+                this.childList = data;
+                console.log(data, 'Standarddata-----222222222222222-----');
             }
         }
     }
@@ -32,5 +58,58 @@ export default {
   
 <style lang="less" scoped>
 .wrapper {
+    display: flex;
+        
+        justify-content: space-between;
+    .child-table{
+        // width: 48%;
+        // background-color: red;
+        font-size: 14px;
+        text-align: center;
+        color: rgba(255, 255, 255, 1);
+        .table-content{
+            display: flex;
+            line-height: 40px;
+            
+            .item{
+                background-color: rgba(96, 104, 119, 1);
+                height: 40px;
+                border: 1px solid rgb(171, 176, 187);
+                &.man-sex{
+                    width: 60px;
+                    background-color: rgba(1, 84, 120, 0.69);
+                }
+                &.woman-sex{
+                    width: 60px;
+                    background-color: rgba(236, 128, 141, 0.75);
+                }
+                &.height{
+                    width: 80px;
+                }
+                &.width{
+                    width: 80px;
+                }
+                &.head-man-height{
+                    width: 80px;
+                    background-color: rgba(1, 84, 120, 0.69);
+                }
+                &.head-man-width{
+                    width: 80px;
+                    background-color: rgba(1, 84, 120, 0.69);
+                }
+                &.head-woman-height{
+                    width: 80px;
+                    background-color: rgba(236, 128, 141, 0.75);
+                }
+                &.head-woman-width{
+                    width: 80px;
+                    background-color: rgba(236, 128, 141, 0.75);
+                }
+            }
+     
+
+        }
+    }
+    
 }
 </style>
