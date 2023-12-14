d297a9eb284ba252705caa9dff75ca548dc05c7e
chjiahao <1294979952@qq.com>
2023-12-15 02:45:24 +0800

    光盘小达人

---
+++ src/views/Food/components/EatItem.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="eat-item-box">
+        <div class="eat-index">{{ eatData.index }}</div>
+        <img src="" alt="">
+        <div class="e-t-content">
+            <div class="c-top">
+                <div class="name">{{eatData.studentName}}</div>
+                <div class="num">{{ eatData.cdNum }}次</div>
+            </div>
+            <div class="c-bottom">
+                <el-progress  :stroke-width="10" :show-text="false" :percentage="100" 
+                :color="index==0?'rgba(243, 90, 76, 1)':(index==1?'rgba(234, 156, 71, 1)':'rgba(62, 205, 126, 1)')"
+                >
+                </el-progress>
+            </div>
+            
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "EatItem",
+    components: { },
+    props: {
+        eatData: {
+            type: Object,
+            default: () => {
+                return {}
+            }
+        },
+        index: {
+            type: Number,
+            default: () => {
+                return 0
+            }
+        },
+        
+    },
+    data() {
+        return {
+            
+        }
+    },
+    watch: {
+        
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        
+    }
+
+}
+</script>
+  
+<style lang="less" scoped>
+.eat-item-box {
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: rgba(0, 102, 153, 0.5);
+    border-radius: 10px;
+    padding: 0 20px;
+    margin-bottom: 7px;
+    .eat-index{
+        width: 50px;
+        font-size: 28px;
+        font-weight: 700;
+    }
+    img{
+        width: 30px;
+        height: 30px;
+    }
+    .e-t-content{
+        padding: 5px 0;
+        margin-left: 16px;
+        flex-grow: 1;
+        height: 41px;
+        .c-top{
+            display: flex;
+            justify-content: space-between;
+        }
+        .c-bottom{
+         
+        }
+        
+    }
+   
+}
+</style>
--- src/views/Food/components/EatUp.vue
+++ src/views/Food/components/EatUp.vue
@@ -4,17 +4,84 @@
             <SearchForm gradeSearch time  classSearch v-model="formVlaue" />
         </div>
         <div class="content">
+            <div class="content-top">
+                <div class="name">光盘人次</div>
+                <div class="num">222</div>
+                <div class="name">光盘率</div>
+                <div class="redo">
+                    <el-progress type="circle" color="rgba(255, 138, 72, 1)"
+                     :width="90" :stroke-width="8" :percentage="100"></el-progress>
+                </div>
+            </div>
+            <div class="eat-list">
+                <EatItem v-for="(item,index) in eatLists" :key="index" :eatData="item" :index="index"></EatItem>
+            </div>
         </div>
     </div>
 </template>
 <script>
 import { getEatUp } from "@/service/baseInfo.js";
+import EatItem from "./EatItem.vue"
 export default {
     name: "EatUp",
-    components: { },
+    components: { EatItem},
     data() {
         return {
             chartData: {},
+            eatList: [],
+            eatLists:[
+                {
+                    index:1,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:2,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:3,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:4,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:5,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:6,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:7,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:5,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:6,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                {
+                    index:7,
+                    studentName:'陈新飞',
+                    cdNum:'30',
+                },
+                
+            ],
             formVlaue: {
                 gradeId: 'all',
                 timeLabel:'本日',
@@ -42,10 +109,12 @@ export default {
         async getCount() {
             const data = await getEatUp({ gradeId: this.formVlaue.gradeId, startTime: this.formVlaue.timeValue[0], endTime: this.formVlaue.timeValue[1] ,clazzGroupId:this.formVlaue.clazzGroupId})
             if (data) {
-                this.chartData = {
-                    x: data.map(item => item.clazzName),
-                    y: data.map(item => item.cdNum)
-                }
+                // this.chartData = {
+                //     x: data.map(item => item.clazzName),
+                //     y: data.map(item => item.cdNum)
+                // }
+                this.eatList = data
+                console.log(data,'this.chartDatathis.chartData')
             }
         }
     }
@@ -78,6 +147,27 @@ export default {
     .content {
         width: 100%;
         height: 200px;
+        color: rgba(255, 255, 255, 1);
+        font-size: 16px;
+        .content-top{
+            height: 100px;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            margin-bottom: 20px;
+            .num{
+                font-size: 42px;
+                margin-right: 100px;
+            }
+            .redo{
+                height: 100px;
+                width: 100px;
+            }
+        }
+        .eat-list{
+            height: 400px;
+            overflow-y:auto
+        }
     }
 }
 </style>
